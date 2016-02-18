78c3210b8c2c65ac23feb3b396f3ecc8762cfbdc
Matthew J.P. Walker <walkerm930@gmail.com>
2016-02-18 01:47:08 -0500

    ex3: rename some varibles

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -1,18 +1,18 @@
-function p = predict(Theta1, Theta2, X)
+function predictions = predict(Weights1, Weights2, X)
 %PREDICT Predict the label of an input given a trained neural network
-%   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
-%   trained weights of a neural network (Theta1, Theta2)
+%   predictions = PREDICT(Weights1, Weights2, X) outputs the predicted label of X given the
+%   trained weights of a neural network (Weights1, Weights2)
 
 % Useful values
-m = size(X, 1);
-num_labels = size(Theta2, 1);
+data_size = size(X, 1);
+num_labels = size(Weights2, 1);
 
 % You need to return the following variables correctly 
-p = zeros(size(X, 1), 1);
+predictions = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
+%               your learned neural network. You should set prediction to a
 %               vector containing labels between 1 to num_labels.
 %
 % Hint: The max function might come in useful. In particular, the max
@@ -20,12 +20,6 @@ p = zeros(size(X, 1), 1);
 %       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
 %
-
-
-
-
-
-
 
 
 
